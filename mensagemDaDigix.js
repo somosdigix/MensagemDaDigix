b2123cb44bfc98a4ef77e872ddfe0959d3d2d218
Wilson Iwano <wilsoniwano@digix.com.br>
2021-02-25 10:50:07 -0400

    Desabilitando a mensagem

--- mensagemDaDigix.js
+++ mensagemDaDigix.js
@@ -147,5 +147,5 @@ const sistemasComMensagemHabilitada = {
 }
 
 const caminhoDaImagem = 'https://somosdigix.github.io/MensagemDaDigix/mensagem-suporte.png';
-// new MensagemDaDigix(caminhoDaImagem).iniciar();
-new MensagemDaDigix(caminhoDaImagem, sistemasComMensagemHabilitada).iniciar();
+
+// new MensagemDaDigix(caminhoDaImagem, sistemasComMensagemHabilitada).iniciar();
