d5c6e8310251295898d4a3215357c56f5e5fcb9d
FPdigix <87763785+fpdigix@users.noreply.github.com>
2023-09-13 09:23:47 -0300

    Update mensagemDaDigix.js

--- mensagemDaDigix.js
+++ mensagemDaDigix.js
@@ -150,4 +150,4 @@ const sistemasComMensagemHabilitada = [
 ]
 
 const caminhoDaImagem = 'https://somosdigix.github.io/MensagemDaDigix/mensagem-suporte.png';
-new MensagemDaDigix(caminhoDaImagem, sistemasComMensagemHabilitada).iniciar();
+// new MensagemDaDigix(caminhoDaImagem, sistemasComMensagemHabilitada).iniciar();
