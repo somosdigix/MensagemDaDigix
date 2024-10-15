211ec242f2ac25437a21a5c300607d00fb4d66d5
beatrizreisdigix <beatrizreis@digix.com.br>
2024-10-15 09:53:44 -0400

    Update mensagemDaDigix.js

--- mensagemDaDigix.js
+++ mensagemDaDigix.js
@@ -151,5 +151,4 @@ const sistemasComMensagemHabilitada = [
 ]
 
 const caminhoDaImagem = 'https://somosdigix.github.io/MensagemDaDigix/mensagem-suporte.png';
-//new MensagemDaDigix(caminhoDaImagem, sistemasComMensagemHabilitada).iniciar();
-new MensagemDaDigix(caminhoDaImagem, [cheffescolarPiaui]).iniciar();
+new MensagemDaDigix(caminhoDaImagem, sistemasComMensagemHabilitada).iniciar();
